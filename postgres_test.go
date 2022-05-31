b6ebe0c5d67908066334f2d9648ede897bb7c64e
Javier Olaechea <pirata@gmail.com>
2022-05-30 23:24:41 -0500

    dbhelpers

--- postgres_test.go
+++ postgres_test.go
@@ -4,23 +4,26 @@ import (
 	"context"
 	"testing"
 
-	"github.com/jackc/pgx/v4"
 	"github.com/jackc/pgx/v4/pgxpool"
 )
 
-func setuptestdb(t *testing.T) (*pgxpool.Pool, func()) {
+func cleardb(t *testing.T, db *pgxpool.Pool) {
 	t.Helper()
 	ctx := context.TODO()
+	_, err := db.Exec(ctx, "TRUNCATE sessions, users")
+	if err != nil {
+		t.Fatalf("Unable to restart database. %s", err)
+	}
+
+}
+
+func requiresdb(t *testing.T) *pgxpool.Pool {
+	t.Helper()
+	ctx := context.TODO()
+	// todo(javier): Get database name from environment variable
 	pool, err := pgxpool.Connect(ctx, "postgres:///swiki-test")
 	if err != nil {
 		t.Fatalf("Could not connect to database %s", err)
 	}
-	tx, err := pool.BeginTx(context.TODO(), pgx.TxOptions{IsoLevel: pgx.Serializable})
-	if err != nil {
-		t.Fatalf("Failed to acquire the transaction. %s", err)
-	}
-	return pool, func() {
-		tx.Rollback(ctx)
-		pool.Close()
-	}
+	return pool
 }
