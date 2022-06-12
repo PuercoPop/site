303e5abd03b5e8c6e840c87c2737872cf087bf0a
Javier Olaechea <pirata@gmail.com>
2022-06-12 01:00:18 -0500

    Handle interrupt and term signals

--- cmd/site/main.go
+++ cmd/site/main.go
@@ -4,6 +4,9 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/PuercoPop/site"
 )
@@ -14,6 +17,12 @@ var key = flag.String("key", "localhost+2-key.pem", "The keyFile to use for http
 
 func main() {
 	flag.Parse()
+	go func() {
+		sc := make(chan os.Signal, 1)
+		signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+		<-sc
+		os.Exit(0)
+	}()
 	srv := site.New(*dbpath)
 	// TODO(javier): Obtain addr and cert files from flags
 	log.Fatal(http.ListenAndServeTLS(":8080", *cert, *key, srv.Mux))
