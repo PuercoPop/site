2177ef6dc39931af13596eb71cf15910123a61a7
Javier Olaechea <pirata@gmail.com>
2022-08-24 02:15:18 -0500

    Refactor tests using subtests

--- blog/blog_test.go
+++ blog/blog_test.go
@@ -52,40 +52,51 @@ func TestSite(t *testing.T) {
 		t.Fatalf("Could not access subdirectory: %s", err)
 	}
 	site := New(blogdir)
-	// assert tags
-	// The tags are en es testing and blog.
-	var tags []string
-	for t := range site.ByTag {
-		tags = append(tags, t)
-	}
 	less := func(a, b string) bool { return a < b }
-	if diff := cmp.Diff([]string{"en", "es", "testing", "blog"}, tags, cmpopts.SortSlices(less)); diff != "" {
-		t.Errorf("Tag list mismatch (-want, +got): %s", diff)
+	t.Run("A list of tags is assembled", func(t *testing.T) {
+		var tags []string
+		for t := range site.ByTag {
+			tags = append(tags, t)
+		}
+		if diff := cmp.Diff([]string{"en", "es", "testing", "blog"}, tags, cmpopts.SortSlices(less)); diff != "" {
+			t.Errorf("Tag list mismatch (-want, +got): %s", diff)
 
-	}
-	// Test ByTag index
-	var tagtitles []string
-	for _, p := range site.ByTag["en"] {
-		tagtitles = append(tagtitles, p.Title)
-	}
-	if diff := cmp.Diff([]string{"Some title", "Another title"}, tagtitles, cmpopts.SortSlices(less)); diff != "" {
-		t.Errorf("en tag content mismatch (-want, +got): %s", diff)
-	}
+		}
+	})
+	t.Run("Retrieve the posts by tag", func(t *testing.T) {
+
+		var titles []string
+		for _, p := range site.ByTag["en"] {
+			titles = append(titles, p.Title)
+		}
+		diff := cmp.Diff([]string{"Some title", "Another title"}, titles,
+			cmpopts.SortSlices(less))
+		if diff != "" {
+			t.Errorf("en tag content mismatch (-want, +got): %s", diff)
+		}
+	})
 	// Test ByDate index
-	var datetitles []string
-	for _, p := range site.ByDate[civil.Date{Year: 2022, Month: time.March, Day: 30}] {
-		datetitles = append(datetitles, p.Title)
-	}
-	if diff := cmp.Diff([]string{"Some title", "Another title", "Yet another title"}, datetitles, cmpopts.SortSlices(less)); diff != "" {
-		t.Errorf("2022-3-30 date content mismatch (-want, +got): %s", diff)
-	}
+	t.Run("Retrieve the posts by published date", func(t *testing.T) {
+		var titles []string
+		for _, p := range site.ByDate[civil.Date{Year: 2022, Month: time.March, Day: 30}] {
+			titles = append(titles, p.Title)
+		}
+		diff := cmp.Diff([]string{"Some title", "Another title", "Yet another title"}, titles,
+			cmpopts.SortSlices(less))
+		if diff != "" {
+			t.Errorf("2022-3-30 date content mismatch (-want, +got): %s", diff)
+		}
+	})
 	// Test BySlug index
-	slug := "yet-another-title"
-	p := site.BySlug[slug]
-	if p == nil {
-		t.Fatalf("Byslug index mismatch. No post found under '%s'", slug)
-	}
-	if diff := cmp.Diff(p.Title, "Yet another title"); diff != "" {
-		t.Errorf("BySlug index mismatch (-want, +got): %s", diff)
-	}
+	t.Run("Retrieve a post by slug", func(t *testing.T) {
+		slug := "yet-another-title"
+		p := site.BySlug[slug]
+		if p == nil {
+			t.Fatalf("Byslug index mismatch. No post found under '%s'", slug)
+		}
+		if diff := cmp.Diff(p.Title, "Yet another title"); diff != "" {
+			t.Errorf("BySlug index mismatch (-want, +got): %s", diff)
+		}
+
+	})
 }
