d57ba095bdf33e92bab59366d2c761e6320651fd
Javier Olaechea <pirata@gmail.com>
2022-11-12 08:52:59 -0500

    Finish basic site_test.go

--- site_test.go
+++ site_test.go
@@ -25,22 +25,44 @@ func (finsta *FakeFinsta) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("reached finsta"))
 }
 
-func TestMainSite(t *testing.T) {
-	// fake request from blog.puercopop.com
-	// finsta.puercopop.com
-	// WWW.puercopop.com
-	// tt := []struct{
-	// 	desc string
-	// 	url string
-	// 	want string
-	// }
+func TestHostRoutuing(t *testing.T) {
+	tt := []struct {
+		desc string
+		url  string
+		want string
+	}{
+		{
+			desc: "The blog subdomain should be redirected to the blog",
+			url:  "https://blog.puercopop.com",
+			want: "reached fakeblog",
+		},
+		{
+			desc: "The finsta subdomain should be redirected to finsta",
+			url:  "https://finsta.puercopop.com",
+			want: "reached fakefinsta",
+		},
+		{
+			desc: "The www subdomain should be redirected to the main site",
+			url:  "https://www.puercopop.com",
+			want: "reached fakewww",
+		},
+		{
+			desc: "The tld should be redirected to the main site",
+			url:  "https://puercopop.com",
+			want: "reached fakewww",
+		},
+	}
 	srv := site{WWW: &FakeWWW{}, Blog: &FakeBlog{}, Finsta: &FakeFinsta{}}
-	req := httptest.NewRequest(http.MethodGet, "https://blog.puercopop.com", nil)
-	w := httptest.NewRecorder()
-	srv.ServeHTTP(w, req)
-	resp := w.Result()
-	body, _ := io.ReadAll(resp.Body)
-	if diff := cmp.Diff(string(body), "reached fakeblog"); diff != "" {
-		t.Errorf("Response mismatch (-want, +got): %s", diff)
+	for _, tc := range tt {
+		t.Run(tc.desc, func(t *testing.T) {
+			req := httptest.NewRequest(http.MethodGet, tc.url, nil)
+			w := httptest.NewRecorder()
+			srv.ServeHTTP(w, req)
+			resp := w.Result()
+			body, _ := io.ReadAll(resp.Body)
+			if diff := cmp.Diff(string(body), tc.want); diff != "" {
+				t.Errorf("Response mismatch (-want, +got): %s", diff)
+			}
+		})
 	}
 }
