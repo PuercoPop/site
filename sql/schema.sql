a4e80664d529c688e11269802a773122ce86481b
Javier Olaechea <pirata@gmail.com>
2022-11-18 09:48:20 -0500

    Some more work on the schema

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,19 +1,51 @@
 -- -*- sql-product: postgres -*-
 BEGIN;
 
-CREATE TABLE users (
-       user_id INTEGER PRIMARY KEY generated always as identity,
-       email TEXT NOT NULL UNIQUE, -- todo(javier): we want uniqueness to be case-insensitive
-       password BYTEA NOT NULL,
-       admin BOOLEAN NOT NULL DEFAULT false
+CREATE SCHEMA blog IF NOT EXISTS;
+
+CREATE SCHEMA finsta IF NOT EXISTS;
+
+CREATE EXTENSION IF NOT EXISTS pg_stats;
+
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
+CREATE EXTENSION IF NOT EXISTS citext;
+
+-- create public.migrations ()
+CREATE TABLE blog.posts (
+  post_id integer GENERATED always AS IDENTITY PRIMARY KEY,
+  title text NOT NULL,
+  slug text NOT NULL,
+  path text NOT NULL,
+  published_at time WITH time zone
 );
 
-create table sessions (
-       session_id BYTEA PRIMARY KEY,
-       user_id INTEGER NOT NULL,
-       created_at TIME WITH TIME ZONE NOT NULL DEFAULT NOW(),
-       expires_at TIME WITH TIME ZONE,
-       foreign key (user_id) references users(user_id)
+CREATE TABLE blog.tags (
+  tag text NOT NULL PRIMARY KEY
+);
+
+CREATE TABLE blog.post_tags (
+  post_id integer NOT NULL,
+  tag text NOT NULL,
+  FOREIGN KEY (post_id) REFERENCES blog.posts (post_id),
+  FOREIGN KEY (tag) REFERENCES blog.tags (tag),
+  PRIMARY KEY (post, tag)
+);
+
+-- finsta
+CREATE TABLE users (
+  user_id integer PRIMARY KEY GENERATED always AS IDENTITY,
+  email text NOT NULL UNIQUE, -- todo(javier): we want uniqueness to be case-insensitive
+  password BYTEA NOT NULL,
+  admin boolean NOT NULL DEFAULT FALSE
+);
+
+CREATE TABLE sessions (
+  session_id bytea PRIMARY KEY,
+  user_id integer NOT NULL,
+  created_at time WITH time zone NOT NULL DEFAULT NOW(),
+  expires_at time WITH time zone,
+  FOREIGN KEY (user_id) REFERENCES users (user_id)
 );
 
 -- CREATE TABLE content_types (
@@ -22,7 +54,6 @@ create table sessions (
 -- );
 -- INSERT INTO content_types (name)
 -- VALUES ('note'), ('photo'), ('article');
-
 -- CREATE table posts (
 --     page_id INTEGER PRIMARY KEY,
 --     title TEXT NOT NULL, -- slug?
