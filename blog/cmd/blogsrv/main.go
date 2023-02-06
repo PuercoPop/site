489ccb597710358b0d39d64ca58b5394e5f76bb4
Javier Olaechea <pirata@gmail.com>
2023-02-06 00:06:17 -0500

    Move database pool initialization logic to the blog srv cmd
    THe only place where it is going to be used

--- blog/cmd/blogsrv/main.go
+++ blog/cmd/blogsrv/main.go
@@ -1,5 +1,27 @@
 package main
 
+import (
+	"context"
+	"log"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func NewDB(ctx context.Context, url string) (*pgxpool.Pool, error) {
+	conf, err := pgxpool.ParseConfig(url)
+	if err != nil {
+		log.Fatalf("Could not parse the PG connection URL. %s", err)
+	}
+	db, err := pgxpool.NewWithConfig(ctx, conf)
+	if err != nil {
+		log.Fatalf("%s", err)
+	}
+	// TODO(javier): ping the db
+	// func (p *Pool) Ping(ctx context.Context) error
+	return db, nil
+}
+
 func main() {
+	ctx := context.Background()
 
 }
