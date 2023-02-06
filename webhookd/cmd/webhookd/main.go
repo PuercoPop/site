5408db4e426ee76f0cf32d51a0114e70e54cba91
Javier Olaechea <pirata@gmail.com>
2023-02-05 23:04:54 -0500

    move webhookd executable

--- webhookd/cmd/webhookd/main.go
+++ webhookd/cmd/webhookd/main.go
@@ -1,6 +1,6 @@
 // webhookd listens for webhooks from GitHub. GitHub will send a notification
 // everytime a new commit lands on the default branch. Upon receiving the
-// notification webhookd will rebuild all the swiki cmds.
+// notification webhookd will update the site.
 package main
 
 import (
