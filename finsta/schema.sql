44e52327b9fb9e523b1146c7c7482837f0f7699b
Javier Olaechea <pirata@gmail.com>
2023-02-15 14:15:03 -0500

    finsta schema wip

---
+++ finsta/schema.sql
@@ -0,0 +1,39 @@
+-- -*- sql-product: postgres -*-
+begin;
+
+drop schema finsta cascade all;
+create schema finsta if not exists;
+CREATE TABLE finsta.users (
+  user_id integer PRIMARY KEY GENERATED always AS IDENTITY,
+  email text NOT NULL UNIQUE, -- todo(javier): we want uniqueness to be case-insensitive
+  password BYTEA NOT NULL,
+  admin boolean NOT NULL DEFAULT FALSE
+);
+
+CREATE TABLE finsta.sessions (
+  session_id bytea PRIMARY KEY,
+  user_id integer NOT NULL,
+  created_at time WITH time zone NOT NULL DEFAULT NOW(),
+  expires_at time WITH time zone,
+  FOREIGN KEY (user_id) REFERENCES users (user_id)
+);
+
+--- Use cases
+-- 1. Upload an image to the archive of the user
+
+-- CREATE TABLE content_types (
+--     content_type_id INTEGER PRIMARY KEY,
+--     name TEXT NOT NULL UNIQUE
+-- );
+-- INSERT INTO content_types (name)
+-- VALUES ('note'), ('photo'), ('article');
+-- CREATE table posts (
+--     page_id INTEGER PRIMARY KEY,
+--     title TEXT NOT NULL, -- slug?
+--     content TEXT NOT NULL,
+--     published_at date not null default now(),
+--     CREATED_AT DATETIME NOT NULL DEFAULT NOW()
+--     -- author_id integer references
+-- )
+create table finsta.media ();
+commit;
