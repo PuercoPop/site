c99ca71fd4e81ac418e41d4b669d8f93b36f753e
Javier Olaechea <pirata@gmail.com>
2023-02-15 14:14:52 -0500

    Continue working on blog implementation

--- blog/src/main.rs
+++ blog/src/main.rs
@@ -1,3 +1,16 @@
+#[derive(Debug)]
+struct Post {
+    title: String,
+    // pubdate: chrono::NaiveDate,
+    // draft: boolean,
+    tags: Vec<Tag>
+}
+
+#[derive(Debug)]
+struct Tag {
+    name: String
+}
+
 fn main() {
     println!("Hello, world!");
 }
