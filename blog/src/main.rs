c0dd877f5de34ec514e8ffb607a7ee470f860e80
Javier Olaechea <pirata@gmail.com>
2023-02-15 20:00:39 -0500

    Tests passing

--- blog/src/main.rs
+++ blog/src/main.rs
@@ -1,8 +1,8 @@
-use::std::result::Result::Err;
+// use::std::result::Result::Err;
 use::std::path::Path;
 
 #[derive(Debug)]
-struct Post {
+pub struct Post {
     title: String,
     pubdate: chrono::NaiveDate,
     draft: bool,
@@ -14,8 +14,10 @@ struct Tag {
     name: String
 }
 
-pub fn read_post(path: &Path) -> Result<Post, Err(String)> {
-    return Ok(Post{})
+pub fn read_post(path: &Path) -> Result<Post, ()> {
+    let tags: Vec<Tag> = Vec::new();
+    let pubdate = chrono::NaiveDate::from_ymd_opt(2023, 2, 15).unwrap();
+    return Ok(Post{title: "".to_string(), pubdate: pubdate, draft: true, tags: tags})
 }
 
 fn main() {
@@ -28,7 +30,7 @@ mod tests {
     #[test]
     fn test_draft_1() {
         let path = Path::new("./testdata/draft_01.md");
-        let post = read_post(path);
+        let post = read_post(path).expect("Could not read post");
         assert_eq!(post.draft, true)
     }
     // fn test_draft_2() {
