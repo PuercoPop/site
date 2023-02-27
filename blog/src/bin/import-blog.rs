3b095c891a807ccab55b3a41059a1d84c86e55ff
Javier Olaechea <pirata@gmail.com>
2023-02-27 15:28:46 -0500

    Rename main.rs to lib.rs
    The code in main.rs is going to be linked against multiple executables

--- blog/src/bin/import-blog.rs
+++ blog/src/bin/import-blog.rs
@@ -1,26 +1,38 @@
 // import-blog [directory]
-use std::fs;
+use blog::read_post;
 use clap::Parser;
+use std::fs;
+use std::io;
+use std::path::Path;
 
 #[derive(Parser, Debug)]
 struct Opts {
-     // TODO(javier): Figure out how to pass values as environment variables as
-     // well.
-    #[arg(short = "D")]
+    // TODO(javier): Figure out how to pass values as environment variables as
+    // well.
+    #[arg(short = 'D')]
     dir: String,
-    #[arg(short = "d")]
-    dburl: String,
-
+    // #[arg(short = 'd')]
+    // dburl: String,
 }
 
-fn main() -> io::Result<()> {
+fn main() -> Result<(), io::Error> {
     // 1. Iterate over the directory
     // 2. Filter out the posts that start with draft: in the header
     // 3. Insert the remaining posts using the path to determine whether the
     //    post already exists in the database.
     let args = Opts::parse();
-    fs::read_dir(args.dir)?;
+    let dir = &args.dir.to_owned();
+    let path = Path::new(dir);
+    for entry in fs::read_dir(path)? {
+        let entry = entry?;
+        let metadata = entry.metadata()?;
+        // TODO(javier): Replace with is_post()
+        if !metadata.is_dir() {
+            let post = read_post(&entry.path());
+            println!("post: {post:#?}");
+        }
+    }
 
     // let (client, conn) = tokio_postgres::connect("", ).await?;
-
+    Ok(())
 }
