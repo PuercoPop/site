245a4d933a58dc73cfddcc949690251f6d4d9084
Javier Olaechea <pirata@gmail.com>
2023-04-14 22:42:12 -0500

    Serve a string

--- finsta/src/main.rs
+++ finsta/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{response::Html, routing::get, Router};
+
+async fn index() -> Html<&'static str> {
+    Html("こんにちは")
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(index));
+    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
