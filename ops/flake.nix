7d5cccef0b85a49469e9761dedc866b03bd42047
Javier Olaechea <pirata@gmail.com>
2023-06-03 23:43:43 -0500

    Add devshell for ops

---
+++ ops/flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "Provision and deploy `site'";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+  outputs = { self, nixpkgs}:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+      {
+        devShells.${system}.default = pkgs.mkShell {
+          buildInputs = [pkgs.terraform pkgs.terraform-ls pkgs.nixos-rebuild];
+        };
+      };
+}
