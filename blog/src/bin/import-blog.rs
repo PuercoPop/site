ca308d8abfe41e59025dd5e9aa9fef339d668d0c
Javier Olaechea <pirata@gmail.com>
2023-07-13 21:18:03 -0500

    [import-blog]: Add tag entry for each post and clean up unused tags

--- blog/src/bin/import-blog.rs
+++ blog/src/bin/import-blog.rs
@@ -18,11 +18,14 @@ struct Opts {
 
 static INSERT_POST_QUERY: &str = "INSERT INTO blog.posts (title, published_at, draft, content, path)
 VALUES ($1, $2, $3, $4, $5) ON CONFLICT (path) DO UPDATE SET
-title = EXCLUDED.title, published_at = EXCLUDED.published_at, draft = EXCLUDED.draft, content = EXCLUDED.content";
-
+title = EXCLUDED.title, published_at = EXCLUDED.published_at, draft = EXCLUDED.draft, content = EXCLUDED.content
+RETURNING post_id";
 static INSERT_TAGS_QUERY: &str = "INSERT INTO blog.tags (TAG) VALUES ($1) ON CONFLICT DO NOTHING";
-static _INSERT_POST_TAGS_QUERY: &str = "INSERT INTO blog.post_tags (post_id, tag) VALUES ($1, $2)";
-static _REMOVE_UNUSED_POST_TAGS_QUERY: &str = "DELETE FROM blog.tags WHERE";
+static INSERT_POST_TAGS_QUERY: &str = "INSERT INTO blog.post_tags (post_id, tag) VALUES ($1, $2) ON CONFLICT DO NOTHING";
+static REMOVE_UNUSED_POST_TAGS_QUERY: &str = "WITH unused AS (
+SELECT tag FROM blog.tags NATURAL LEFT OUTER JOIN blog.post_tags WHERE post_id IS NULL
+)
+DELETE FROM blog.tags WHERE tag IN (SELECT tag from unused)";
 
 
 // TODO(javier): Move function to lib.rs
@@ -30,7 +33,7 @@ fn store_post(client: &mut Client, post: blog::Post) {
     let stmt = client
         .prepare(INSERT_POST_QUERY)
         .expect("Could not prepare the statement");
-    let _ret = client
+    let rows = client
         .query(
             &stmt,
             &[
@@ -42,10 +45,12 @@ fn store_post(client: &mut Client, post: blog::Post) {
             ],
         )
         .expect("Unable to insert post entry.");
-    // TODO: then insert each tag
+    let post_id: i32 = rows.iter().next().unwrap().get(0);
     let stmt = client.prepare(INSERT_TAGS_QUERY).expect("Could not prepare second statement");
+    let insert_post_tags_stmt = client.prepare(INSERT_POST_TAGS_QUERY).unwrap();
     for tag in post.tags {
         let _ret = client.query(&stmt, &[&tag]).expect("Could not insert tag");
+        let _ret = client.query(&insert_post_tags_stmt, &[&post_id, &tag]).unwrap();
 
     }
 
@@ -91,6 +96,7 @@ fn main() -> Result<(), Error> {
             }
         }
     }
-    // TODO(javier): Clean up any tags that are not used by any post.
+    let stmt = client.prepare(REMOVE_UNUSED_POST_TAGS_QUERY).unwrap();
+    let _ret = client.query(&stmt, &[]).unwrap();
     Ok(())
 }
