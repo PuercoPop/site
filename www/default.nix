9332b1c2e134d5887362ab718584b7ddf5a673e5
Javier Olaechea <pirata@gmail.com>
2023-09-17 16:03:58 -0500

    Add www/default.nix

---
+++ www/default.nix
@@ -0,0 +1,17 @@
+{ system ? builtins.currentSystem
+, nixpkgs ? fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/refs/tags/23.05.tar.gz";
+    sha256 = "10wn0l08j9lgqcw8177nh2ljrnxdrpri7bp0g7nvrsn9rkawvlbf";
+  }
+, pkgs ? import nixpkgs {
+    overlays = [ ];
+    config = { };
+    inherit system;
+  }
+}:
+let
+  www = ./resources;
+in
+{
+  www = www;
+}
