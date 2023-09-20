f7b4c9697bc48993d623458c64ed4ef508332d4c
Javier Olaechea <pirata@gmail.com>
2023-09-19 23:20:28 -0500

    Fix SSL setup

--- ops/kraken-configuration.nix
+++ ops/kraken-configuration.nix
@@ -1,4 +1,4 @@
-{ modulesPath, www,... }: {
+{ modulesPath, www, ... }: {
   # TODO: Check if I can use qemu-guest.nix
   imports = [ "${modulesPath}/virtualisation/digital-ocean-image.nix" ];
 
@@ -11,33 +11,37 @@
       enable = true;
       wheelNeedsPassword = false;
     };
-
     acme = {
       acceptTerms = true;
       defaults.email = "pirata+puercopop@gmail.com";
     };
   };
 
-  users.users.nixos = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" "networkmanager" "video" ];
-    # Allow the graphical user to login without password
-    initialHashedPassword = "";
-    openssh.authorizedKeys.keys = [
-      (builtins.fetchurl {
-        url = "https://github.com/PuercoPop.keys";
-        sha256 = "1q50qkjvcn57c5kn0al3097hisv7193jxzs2q1a4cg8hxxyicrp1";
-      })
-    ];
+  users.users = {
+    nixos = {
+      isNormalUser = true;
+      extraGroups = [ "wheel" "networkmanager" "video" ];
+      # Allow the graphical user to login without password
+      initialHashedPassword = "";
+      openssh.authorizedKeys.keys = [
+        (builtins.fetchurl {
+          url = "https://github.com/PuercoPop.keys";
+          sha256 = "1q50qkjvcn57c5kn0al3097hisv7193jxzs2q1a4cg8hxxyicrp1";
+        })
+      ];
+    };
   };
-  users.users.nginx.extraGroups = [ "acme" ];
 
   networking.firewall.allowedTCPPorts = [ 80 443 ];
   services = {
-    # https://discourse.nixos.org/t/nixos-nginx-acme-ssl-certificates-for-multiple-domains/19608/3
     nginx = {
       enable = true;
       virtualHosts = {
+        "puercopop.com" = {
+          forceSSL = true;
+          enableACME = true;
+          globalRedirect = "www.puercopop.com";
+        };
         "www.puercopop.com" = {
           forceSSL = true;
           enableACME = true;
