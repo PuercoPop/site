7978af1a770e2a15b3fd013fcd2d8b57cb076179
Javier Olaechea <pirata@gmail.com>
2023-10-15 09:36:59 -0500

    Pass crane when importing blog form the top-level flake

--- flake.nix
+++ flake.nix
@@ -2,8 +2,12 @@
   description = "NixOS configurations for puercopop.com and hiippo.com";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/release-23.05";
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, crane }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs {
@@ -11,7 +15,10 @@
         overlays = [ ];
         config = { };
       };
-      blog = pkgs.callPackage ./blog { };
+      crane-pkgs = import crane {
+        pkgs = pkgs;
+      };
+      blog = pkgs.callPackage ./blog { pkgs = pkgs; crane = crane-pkgs; };
       www = pkgs.callPackage ./www { };
       # TODO: Use callPackage
       ops = import ./ops {
