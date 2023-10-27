4262898b21c814e914b82e28eddeb2194284583b
Javier Olaechea <pirata@gmail.com>
2023-10-26 23:20:18 -0500

    Print field name

--- finsta/src/lib.rs
+++ finsta/src/lib.rs
@@ -6,7 +6,7 @@ pub async fn index() -> Html<&'static str> {
 
 pub async fn form() -> Html<&'static str> {
     Html("<html><body><h1>Upload Image</h1><form action=/upload method=post enctype='multipart/form-data'>
-<input type=file />
+<input name=media type=file />
 <input type=submit value='upload'/>
 </form></body></html>")
 }
@@ -15,6 +15,6 @@ pub async fn upload(mut multipart: Multipart) {
     // TODO: Replace unwrap calls
     while let Some(field) = multipart.next_field().await.unwrap() {
         let name = field.name().unwrap().to_string();
-        println!("Field is is {}", name);
+        println!("Field is {}", name);
     }
 }
