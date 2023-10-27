c0464021b709a595755be1a15a4489c4d6c1ff1f
Javier Olaechea <pirata@gmail.com>
2023-10-26 23:25:22 -0500

    Retrieve file name

--- finsta/src/lib.rs
+++ finsta/src/lib.rs
@@ -12,9 +12,11 @@ pub async fn form() -> Html<&'static str> {
 }
 
 pub async fn upload(mut multipart: Multipart) {
-    // TODO: Replace unwrap calls
+    // TODO: Replace unwrap calls with ?
     while let Some(field) = multipart.next_field().await.unwrap() {
         let name = field.name().unwrap().to_string();
         println!("Field is {}", name);
+        println!("Filename is {}", field.file_name().unwrap().to_string());
+        // println!("Content Type is {}", field.content_type()?.to_string());
     }
 }
