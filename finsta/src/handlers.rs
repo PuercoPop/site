937972abce68365bc568ec3844f447ced6fbbafd
Javier Olaechea <pirata@gmail.com>
2023-10-30 22:40:35 -0500

    finsta boilerplate

---
+++ finsta/src/handlers.rs
@@ -0,0 +1,25 @@
+use axum::{extract::Multipart, response::Html};
+
+pub async fn index() -> Html<&'static str> {
+    Html("こんにちは<a href=/upload>Upload</a>")
+}
+
+// TODO: whoami page/endpoint
+
+pub async fn form() -> Html<&'static str> {
+    Html("<html><body><h1>Upload Image</h1><form action=/upload method=post enctype='multipart/form-data'>
+<input name=media type=file />
+<input type=submit value='upload'/>
+</form></body></html>")
+}
+
+pub async fn upload(mut multipart: Multipart) {
+    // TODO: Replace unwrap calls with ?
+    // Handler should return something like Result<(), axum::http::StatusCode>
+    while let Some(field) = multipart.next_field().await.unwrap() {
+        let name = field.name().unwrap().to_string();
+        println!("Field is {}", name);
+        println!("Filename is {}", field.file_name().unwrap().to_string());
+        // println!("Content Type is {}", field.content_type()?.to_string());
+    }
+}
--- finsta/src/lib.rs
+++ finsta/src/lib.rs
@@ -1,37 +1,14 @@
 use axum::{
-    extract::Multipart,
-    response::Html,
     routing::{get, post},
     Router,
 };
 
+mod db;
+mod handlers;
+
 pub fn new() -> Router {
     Router::new()
-        .route("/", get(index))
-        .route("/upload", get(form))
-        .route("/upload", post(upload))
-}
-
-pub async fn index() -> Html<&'static str> {
-    Html("こんにちは<a href=/upload>Upload</a>")
-}
-
-// TODO: whoami page/endpoint
-
-pub async fn form() -> Html<&'static str> {
-    Html("<html><body><h1>Upload Image</h1><form action=/upload method=post enctype='multipart/form-data'>
-<input name=media type=file />
-<input type=submit value='upload'/>
-</form></body></html>")
-}
-
-pub async fn upload(mut multipart: Multipart) {
-    // TODO: Replace unwrap calls with ?
-    // Handler should return something like Result<(), axum::http::StatusCode>
-    while let Some(field) = multipart.next_field().await.unwrap() {
-        let name = field.name().unwrap().to_string();
-        println!("Field is {}", name);
-        println!("Filename is {}", field.file_name().unwrap().to_string());
-        // println!("Content Type is {}", field.content_type()?.to_string());
-    }
+        .route("/", get(handlers::index))
+        .route("/upload", get(handlers::form))
+        .route("/upload", post(handlers::upload))
 }
