a96370194cfde8625e0ceb60719d5920bd8b6785
Javier Olaechea <pirata@gmail.com>
2024-01-07 16:38:18 -0500

    Update to latest axum

--- finsta/src/bin/serve.rs
+++ finsta/src/bin/serve.rs
@@ -32,8 +32,9 @@ async fn main() -> Result<(), Error> {
     tracing_subscriber::fmt::init();
 
     let app = new(args.templates_dir, client).layer(TraceLayer::new_for_http());
-    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
-        .serve(app.into_make_service())
-        .await?;
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app.into_make_service())
+        .await
+        .unwrap();
     Ok(())
 }
